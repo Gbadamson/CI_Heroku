6576f9b56788a881d556d49a1e717f7f6feaf33a
Gbadamson <gbadamosi.abubakar.95@gmail.com>
2021-02-22 12:12:45 +0100

    something has been added

--- upload.js
+++ upload.js
@@ -1,30 +1,30 @@
 const express = require("express");
-const multer = require("multer")
+const multer = require("multer");
 
 const PORT = 1114;
 
 const lev = express();
 
 const stocker = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, './filpath')
-    },
-    filename: (req, file, cb)=>{
-        cb(null, file.originalname)
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "./filpath");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-const imgUp = multer ({storage: stocker})
+const imgUp = multer({ storage: stocker });
 
 // lev.post('/imgsin', imgUp.single("avatar"), (req, res)=>{
 //     res.status(201).send("Image posted Successfully")
 // })
 
-lev.post('/imgmult', imgUp.array("avatar", 12), (req, res)=>{
-    res.status(201).send("All Images posted successfully")
-})
+lev.post("/imgmult", imgUp.array("avatar", 12), (req, res) => {
+  res.status(201).send("All Images posted successfully");
+  console.log("Image has been successfully posted");
+});
 
-
-lev.listen(PORT, ()=>{
-    console.log(`${PORT} is working fine`)
-})
+lev.listen(PORT, () => {
+  console.log(`${PORT} is working fine`);
+});
